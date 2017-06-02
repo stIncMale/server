81f6a5593b6337b6ac66cee0de57e62a5fa3b241
Valentin Kovalenko <valentin.male.kovalenko@gmail.com>
2017-06-03 01:28:19 +0300

    Remove garbage

--- src/test/java/stinc/male/server/util/logging/TransferableMdcTest.java
+++ src/test/java/stinc/male/server/util/logging/TransferableMdcTest.java
@@ -4,7 +4,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import org.junit.AfterClass;
 import org.junit.Test;
-import org.slf4j.Logger;
 import org.slf4j.MDC;
 import static junit.framework.TestCase.assertNull;
 import static junit.framework.TestCase.assertSame;
